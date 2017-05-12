3a35976f767836f2ba2097ce5b06e930752f2ac0
daniel.yang <daniel.yang@jinjiang.com>
2017-05-12 17:36:50 +0800

    m

---
+++ ModuleA/UIViewController+ModuleAEntity.h
@@ -0,0 +1,15 @@
+//
+//  UIViewController+ModuleAEntity.h
+//  ModuleA
+//
+//  Created by yangdd on 2017/5/12.
+//  Copyright © 2017年 yangdd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (ModuleAEntity)
+
+-(void) openModuleAIndex;
+
+@end
---
+++ ModuleA/UIViewController+ModuleAEntity.m
@@ -0,0 +1,27 @@
+//
+//  UIViewController+ModuleAEntity.m
+//  ModuleA
+//
+//  Created by yangdd on 2017/5/12.
+//  Copyright © 2017年 yangdd. All rights reserved.
+//
+
+#import "UIViewController+ModuleAEntity.h"
+#import <MGJRouter/MGJRouter.h>
+#import "ModuleIndexViewController.h"
+
+
+@implementation UIViewController (ModuleAEntity)
+
+-(void) openModuleAIndex {
+
+    [MGJRouter registerURLPattern:@"mgj://moduleA" toHandler:^(NSDictionary *routerParameters) {
+        NSLog(@"params is %@", routerParameters);
+
+        [self.navigationController pushViewController:[[ModuleIndexViewController alloc] initWithNibName:@"ModuleAXibBundle.bundle/ModuleIndexViewController" bundle:nil] animated:YES];
+        }];
+
+    [MGJRouter openURL:@"mgj://moduleA"];
+}
+
+@end
