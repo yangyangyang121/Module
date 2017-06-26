f4e2bc832d6a2b9f2a9fda1bc5e505724f2b4a58
daniel.yang <daniel.yang@jinjiang.com>
2017-06-26 15:25:46 +0800

    m

---
+++ ModuleA/Target_ModuleA.h
@@ -0,0 +1,16 @@
+//
+//  Target_ModuleA.h
+//  ModuleA
+//
+//  Created by yangdd on 2017/6/26.
+//  Copyright © 2017年 yangdd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface Target_ModuleA : NSObject
+
+-(UIViewController *)action_GetModuleAIndexController:(NSDictionary *)param;
+
+@end
---
+++ ModuleA/Target_ModuleA.m
@@ -0,0 +1,23 @@
+//
+//  Target_ModuleA.m
+//  ModuleA
+//
+//  Created by yangdd on 2017/6/26.
+//  Copyright © 2017年 yangdd. All rights reserved.
+//
+
+#import "Target_ModuleA.h"
+#import "ModuleIndexViewController.h"
+
+@implementation Target_ModuleA
+
+-(UIViewController *)action_GetModuleAIndexController:(NSDictionary *)param {
+    ModuleIndexViewController *vc=[[ModuleIndexViewController alloc]initWithNibName:@"ModuleIndexViewController" bundle:[NSBundle bundleForClass:[self class]]];
+
+    if ([param.allKeys containsObject:@"callBack"]) {
+//        vc.callBack=[param objectForKey:@"callBack"];
+    }
+    return vc;
+}
+
+@end
